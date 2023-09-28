05c7ca4431d9ec29d5ec166713469aca96bf6db9
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-09-29 00:03:23 +0200

    Добавьте файлы проекта.

---
+++ lab1-1Fomichev/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+double F = Math.Cos(x * x + 2) + (3.5 * (x * x))/(Math.Pow(Math.Cos(y), 2));
+Console.WriteLine($"F={F:F2}");
